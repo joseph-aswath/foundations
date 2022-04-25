40cfcc71d6abf2fe6f3fa6c067bc4e9a4fed2e87
joseph-aswath <josehaswath@gmail.com>
2022-04-25 17:01:29 +0530

    last commit of 25 apr 2022

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="main-heading">
+        <h1 >FOUNDATIONS</h1>
+        </div>
+
+        <div className="listOfConcepts">
+        <ol>
+          <li>navigation bar horizontal</li>
+          <li>routing</li>
+          <li>components partitioning</li>
+          <li>http methods : post,put,delete,get</li>
+          <li>connecting to a backend</li>
+          <li>backend to database</li>
+          <li>forms</li>
+          <li>mobile friendly</li>
+          <li>JSON manipulation</li>
+          <li>bundling & production</li>
+        </ol>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
